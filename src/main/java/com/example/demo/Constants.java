376607e1cf06baa19ac5e5dd8047c40ca1da98e7
andrey <gepeg3317@mail.ru>
2025-06-21 14:57:24 +0300

    Add Constants class

---
+++ src/main/java/com/example/demo/Constants.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Constants {
+
+    // Скорость вывода алкоголя (промилле в час)
+    static final double ALCOHOL_ELIMINATION_RATE = 0.15;
+
+    // Мужской коэффициент для формулы Видмарка
+    static final double MALE_COEFFICIENT = 0.7;
+
+    // Женский коэффициент для формулы Видмарка
+    static final double FEMALE_COEFFICIENT = 0.6;
+
+    // Плотность этанола (г/мл)
+    static final double ETHANOL_DENSITY = 0.78924;
+
+}
